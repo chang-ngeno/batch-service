be5c0f29dcb1ab83f1edcef99ab28eaeaae6a51a
Daniel C. <danchangmasa@gmail.com>
2020-10-20 20:44:40 +0300

    Added a simple schedule Logging message in terminal after a specified time.

--- src/main/java/core/batch/config/BatchConfig.java
+++ src/main/java/core/batch/config/BatchConfig.java
@@ -26,66 +26,66 @@ import core.batch.processor.EmployeeProcessor;
 
 @Configuration
 @EnableBatchProcessing
-public class SpringBootBatchConfig {
+public class BatchConfig {
 
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
-	
+
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
-	
+
 	@Autowired
 	public DataSource dataSource;
-	
+
 	@Bean
-	public FlatFileItemReader<Employee> reader(){
+	public FlatFileItemReader<Employee> reader() {
 		FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>();
 		reader.setResource(new ClassPathResource("employee.csv"));
-		
-		reader.setLineMapper(new DefaultLineMapper<Employee>() {{
-			setLineTokenizer(new DelimitedLineTokenizer() {{
-				setNames(new String[] { "first_name", "last_name","company_name","address","city","county","state","zip" });
-			}});
-			setFieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
-				setTargetType(Employee.class);
-			}});
-		}});
-		
+
+		reader.setLineMapper(new DefaultLineMapper<Employee>() {
+			{
+				setLineTokenizer(new DelimitedLineTokenizer() {
+					{
+						setNames(new String[] { "first_name", "last_name", "company_name", "address", "city", "county",
+								"state", "zip" });
+					}
+				});
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<>() {
+					{
+						setTargetType(Employee.class);
+					}
+				});
+			}
+		});
+
 		return reader;
 	}
-	
+
 	@Bean
 	public EmployeeProcessor processor() {
 		return new EmployeeProcessor();
 	}
-	
+
 	@Bean
-    public JdbcBatchItemWriter<EmployeeDTO> writer() {
-        JdbcBatchItemWriter<EmployeeDTO> writer = new JdbcBatchItemWriter<EmployeeDTO>();
-        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-        writer.setSql("INSERT INTO employee (first_name,last_name,company_name,address,city,county,state,zip) " +
-                "VALUES (:firstName, :lastName,:companyName,:address,:city,:county,:state,:zip)");
-        writer.setDataSource(dataSource);
-        return writer;
-    }
+	public JdbcBatchItemWriter<EmployeeDTO> writer() {
+		JdbcBatchItemWriter<EmployeeDTO> writer = new JdbcBatchItemWriter<EmployeeDTO>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+		writer.setSql("INSERT INTO employee (first_name,last_name,company_name,address,city,county,state,zip) "
+				+ "VALUES (:firstName, :lastName,:companyName,:address,:city,:county,:state,:zip)");
+		writer.setDataSource(dataSource);
+		return writer;
+	}
 
-    @Bean
-    public Job importUserJob(JobListener listener) {
-        return jobBuilderFactory.get("importUserJob")
-                .incrementer(new RunIdIncrementer())
-                .listener(listener)
-                .flow(step1())
-                .end()
-                .build();
-    }
+	@Bean
+	public Job importUserJob(JobListener listener) {
+		return jobBuilderFactory.get("importUserJob").incrementer(new RunIdIncrementer()).listener(listener)
+				.flow(step1()).end().build();
+	}
 
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .<Employee, EmployeeDTO> chunk(10)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                .build();
-    }
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1").<Employee, EmployeeDTO>chunk(10).reader(reader()).processor(processor())
+				.writer(writer()).build();
+	}
+
 }
--- src/main/java/core/batch/listener/JobListener.java
+++ src/main/java/core/batch/listener/JobListener.java
@@ -1,36 +1,62 @@
 package core.batch.listener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 
 import core.batch.DTO.EmployeeDTO;
 
-@Component
+@Configuration
 public class JobListener extends JobExecutionListenerSupport {
-    private final JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public JobListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    @Override
-    public void afterJob(JobExecution jobExecution) {
-        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-            System.out.println("In Completion Listener ..");
-            List<EmployeeDTO> results = jdbcTemplate.query("SELECT first_name,last_name,company_name,address,city,county,state,zip FROM employee",
-                    (rs,rowNum)->{
-                        return new EmployeeDTO(rs.getString(1), rs.getString(2),rs.getString(3),rs.getString(4),
-                                rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8));
-                    }
-            );
-            results.forEach(System.out::println);
-        }
-    }
+	@Autowired
+	public JobListener(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			System.out.println("In Completion Listener ..");
+			List<EmployeeDTO> results = jdbcTemplate.query(
+					"SELECT first_name,last_name,company_name,address,city,county,state,zip FROM employee",
+					(rs, rowNum) -> {
+						return new EmployeeDTO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+								rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
+					});
+			results.forEach(System.out::println);
+
+		} else if (jobExecution.getStatus() == BatchStatus.FAILED) {
+			System.out.println("Failed to complete batch job, JobId: " + jobExecution.getJobId());
+		}
+		renameFile();
+	}
+
+	public void renameFile() {
+		File oldFile = new File("");
+		File newFile = new File("employee.csv.bkp");
+		try {
+			oldFile = new ClassPathResource("employee.csv").getFile();
+			newFile = new File(oldFile.getParentFile() + "/employee_" // Category.FORMAT.ordinal(), Category.FORMAT.ordinal(), Locale.UK
+					+ new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()) + ".csv");
+			System.out.println("The file path is: " + newFile.getAbsolutePath());
+			System.out.println("Renaming processed file: " + oldFile + " to: " + newFile);
+			oldFile.renameTo(newFile);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
 }
